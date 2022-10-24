e6837a49b42a25222df77742a5a9af43da0f7c9e
JamisonCleveland <43040811+JamisonCleveland@users.noreply.github.com>
2022-10-24 16:55:09 -0600

    Editing to follow rubric

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const http = require("http");
 
-http.createServer(function(request, response) {    
-   response.writeHead(200, {"Content-Type": "text/html"});
-   response.write("<h1>Testing testing 123</h1>");    
-   response.end();
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(
+        `<h1>Hello NodeJS!</h1>
+        <h3>Jamison Cleveland, p33b442</h3>
+        <p>Running on port 3060</p>`);
+    response.end();
 }).listen(3060);
